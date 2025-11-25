13b0ebcf1384eeea92885ee75688f2ad3d873fe0
Meliani_Mehdi <mehdi.hatachi@gmail.com>
2025-11-25 14:17:31 +0100

    added a stylesheet and a way to load DNA

--- bioinfo.py
+++ bioinfo.py
@@ -1,8 +1,9 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
-
+from PySide6.QtWidgets import QApplication, QWidget, QFileDialog, QMessageBox
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
@@ -15,6 +16,20 @@ class bioInfo(QWidget):
         self.ui = Ui_bioInfo()
         self.ui.setupUi(self)
 
+        # initialization of variables
+        self.sequence = None
+
+
+        # connecting buttons:
+        self.ui.load_btn.clicked.connect(self.load_sequence)
+
+    def load_sequence(self):
+        path, _ = QFileDialog.getOpenFileName(self, "Open FASTA", "", "FASTA Files (*.fasta *.fa *.txt)")
+        if not path:
+            return
+
+        record = SeqIO.read(path, "fasta")
+        self.sequence = str(record.seq)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
